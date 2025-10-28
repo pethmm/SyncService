382006328c7817267fa7757b70675180637dccae
Pety <38808074+pethmm@users.noreply.github.com>
2025-10-28 21:17:47 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/syncservice/main.go
@@ -0,0 +1,23 @@
+// cmd/syncservice/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "syncservice/internal/syncservice"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := syncservice.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
